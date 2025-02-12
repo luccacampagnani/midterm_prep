90990d45a0f227e82ab5c1c574923161944fe393
luccacampagnani <lcampagnani.ieu2023@student.ie.edu>
2025-02-12 17:59:48 +0100

    added multiple of 6

--- exam_recap.py
+++ exam_recap.py
@@ -34,3 +34,27 @@ def find_words(filename):
                     print(word)
 
 find_words("input.txt")
+
+
+
+def get_multiple():
+    """
+    Returns a multiple of 6 that was entered by the user
+    :return: int a number
+    """
+    while True:
+        try:
+            n = input("Please give me a multiple of 6: ")
+            n = int(n) # convert to int
+            #check if it is multiple of 6
+            if n % 6 == 0:
+                return n
+            #another way
+            if n/6 == n // 6:
+                return n
+            else:
+                print("that is not a multiple of 6")
+        except: ValueError
+        print("you have not entered a number")
+
+get_multiple()
